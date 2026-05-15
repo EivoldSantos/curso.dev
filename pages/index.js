e4e009b38b880faf8a79234b5281b3da11e6092f
Eivold <160683610+EivoldSantos@users.noreply.github.com>
2026-05-15 20:50:20 +0000

    Adiciona o `Prettier` ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Vyttor é gay</h1>
+  return <h1>Vyttor é gay</h1>;
 }
 
 export default Home;
+
+function Teste() {
+  console.log("Teste");
+}
+
+function Teste2() {
+  console.log("Teste2");
+}
