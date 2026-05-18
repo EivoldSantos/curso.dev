2810089c9f8016307f5a647ff30fcd7aa995db88
Eivold <160683610+EivoldSantos@users.noreply.github.com>
2026-05-18 22:14:43 +0000

    Alterado a frase do Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 function Home() {
-  return <h1>Vyttor é gay</h1>;
+  return <h1>Oi mor, tudo bem?</h1>;
 }
 
 export default Home;
-
-function Teste() {
-  console.log("Teste");
-}
-
-function Teste2() {
-  console.log("Teste2");
-}
